7af39360ee0a03137b8b056d46cb7118c0cd5c77
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2020-09-16 10:16:39 +0200

    Added hooks

---
+++ hooks/post_gen_hooks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+
+# Code from https://github.com/audreyfeldroy/cookiecutter-pypackage
+
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def remove_file(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+if __name__ == '__main__':
+    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
+        remove_file('LICENSE')
